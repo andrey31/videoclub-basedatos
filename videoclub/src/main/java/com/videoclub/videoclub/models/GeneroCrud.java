ad45e86448cb6f2ab8e27aed0080449e51484c86
andre <andre@DESKTOP-83IGR0O>
2019-07-14 20:15:26 -0600

    Agrega clases nuevas
    agrega clase GeneroCrud, LenguajeCrud en el paquete models 
    agrega clase Genero, Lenguaje en el paquete entities

---
+++ videoclub/src/main/java/com/videoclub/videoclub/models/GeneroCrud.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.videoclub.videoclub.models;
+
+import com.videoclub.videoclub.models.entities.Genero;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author andre
+ */
+public class GeneroCrud {
+
+    private Conexion conexion = new Conexion();
+
+    /* Elimina un genero por id */
+    public Integer deleteGenero(int id) throws SQLException {
+        conexion.connect();
+        Connection connection = conexion.getConnection();
+
+        String query = "DELETE FROM generos WHERE id = ?";
+
+        PreparedStatement ps = connection.prepareStatement(query);
+
+        ps.setInt(1, id);
+
+        //Retorna 1 si se ejecuta correctamente
+        int delete = ps.executeUpdate();
+        conexion.closeConnection();
+
+        return delete;
+    }
+
+    /* Busca un genero por id */
+    public Genero findByIdGenero(int id) throws SQLException {
+        conexion.connect();
+        Connection connection = conexion.getConnection();
+
+        String query = "SELECT * FROM generos WHERE id = ?";
+
+        PreparedStatement ps = connection.prepareStatement(query);
+        ps.setInt(1, id);
+
+        ResultSet rs = ps.executeQuery();
+
+        if (rs.next()) {
+            Genero genero = new Genero(
+                    rs.getInt("id"),
+                    rs.getString("genero")
+            );
+            conexion.closeConnection();
+            return genero;
+        }
+        conexion.closeConnection();
+        return null;
+    }
+
+    /* Guarda genero  */
+    public Integer saveGenero(Genero genero) throws SQLException {
+        conexion.connect();
+        Connection connection = conexion.getConnection();
+
+        String query = "INSERT INTO generos (genero) VALUES (?)";
+
+        PreparedStatement ps = connection.prepareStatement(query);
+
+        ps.setString(1, genero.getGenero());
+
+        int insert = ps.executeUpdate();
+
+        conexion.closeConnection();
+
+        return insert;
+
+    }
+
+    /* Lista todo los generos */
+    public List<Genero> findAllGenero() throws SQLException {
+        conexion.connect();
+        Connection connection = conexion.getConnection();
+        String query = "SELECT * FROM generos";
+        ResultSet rs = connection.prepareStatement(query).executeQuery();
+        List<Genero> generos = new ArrayList<>();
+
+        while (rs.next()) {
+            Genero genero = new Genero(
+                    rs.getInt("id"),
+                    rs.getString("Genero")
+            );
+            generos.add(genero);
+        }
+        conexion.closeConnection();
+        return generos;
+    }
+
+    /* Actualiza datos de genero por id */
+    public Integer updateGenero(int id, Genero genero) throws SQLException {
+        conexion.connect();
+
+        Connection connection = conexion.getConnection();
+
+        String query = "UPDATE autores SET autor = ? WHERE id = ?";
+        PreparedStatement ps = connection.prepareStatement(query);
+
+        ps.setString(1, genero.getGenero());
+        ps.setInt(2, id);
+        /* Retorna 1 si se ejecuta correctamente */
+        int update = ps.executeUpdate();
+        conexion.closeConnection();
+
+        return update;
+    }
+}
---
+++ videoclub/src/main/java/com/videoclub/videoclub/models/entities/Lenguaje.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.videoclub.videoclub.models.entities;
+
+/**
+ *
+ * @author andre
+ */
+public class Lenguaje {
+      private int id;
+    private String lenguaje;
+
+
+    public Lenguaje() {
+    }
+
+    public Lenguaje(int id, String lenguaje) {
+        this.id = id;
+        this.lenguaje = lenguaje;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLenguaje() {
+        return this.lenguaje;
+    }
+}
