1cbe0908c8539a20af81abe5135a87dd386a86ff
andrey31 <andreybm31@gmail.com>
2019-07-16 20:29:10 -0600

    Agregado paquete icones-awesome

---
+++ videoclub-front/src/plugins/awesome-icons.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+
+import 'vue-awesome/icons/shopping-cart'
+import 'vue-awesome/icons/download'
+import 'vue-awesome/icons/expand-arrows-alt'
+import 'vue-awesome/icons/trash-alt'
+import 'vue-awesome/icons/tasks'
+import 'vue-awesome/icons/cloud-upload-alt'
+import 'vue-awesome/icons/edit'
+
+import Icon from 'vue-awesome/components/Icon'
+
+Vue.component('v-icon', Icon)
