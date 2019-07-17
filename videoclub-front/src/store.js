f1f544d7e7d0e247bf7ecb46cf2a02e2d8fd68a4
andrey31 <andreybm31@gmail.com>
2019-07-16 20:35:17 -0600

    Agregado variables compartidas entre vistas

--- videoclub-front/src/store.js
+++ videoclub-front/src/store.js
@@ -1,16 +1,35 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import services from './services/services'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    services,
+    movies: [],
+    moviesFilter: [],
+    alquiler: {alquiler: false, variant: 'primary'},
+    client: {},
+    shopping: []
   },
-  mutations: {
 
+  mutations: {
+    setMovies: (state, e) => state.movies = e,
+    setMoviesFilter: (state, e) => state.moviesFilter = e,
+    setAlquiler: (state, e) => state.alquiler = e,
+    setClient: (state, e) => state.client = e,
+    setShopping: (state, e) => state.shopping.push(e),
+    clearShopping: (state) => state.shopping = []
   },
   actions: {
-
+    getAllMovie: function({commit}){
+      services.movieService.getAllMovie().then( response => {
+        commit('setMovies', response.data)
+      }).catch( error => {
+        console.log(error)
+      })
+    }
   }
 })
